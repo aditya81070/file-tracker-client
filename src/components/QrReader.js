789ecb5307b2bc31a9ef3a98f81c8a22a107b6bc
Muskan1411 <muskan99ag@gmail.com>
2020-01-11 16:31:51 +0530

    Add QR reader

---
+++ src/components/QrReader.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
+ 
+export default class Qr extends Component {
+  state = {
+    result: 'No result'
+  }
+ 
+  handleScan = data => {
+    if (data) {
+      console.log(data)
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div>
+        <QrReader
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '50%' }}
+        />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
