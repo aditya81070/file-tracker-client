70e3345afcc26784905c53722de4b0b8253f0476
Muskan1411 <muskan99ag@gmail.com>
2020-01-14 19:43:54 +0530

    Add file form by qrg

---
+++ src/components/FileList.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import { withStyles } from "@material-ui/core";
+import { tableIcons } from './TableIcons';
+
+
+const styles = theme => ({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    "& > *": {
+      marginTop: theme.spacing(1.5)
+    },
+    textAlign: "center"
+  },
+  title: {
+    padding: `${theme.spacing(1.25)}px 0px`
+  },
+  button: {
+    textTransform: "none",
+    width: '20%',
+    margin: '10px auto'
+  }
+});
+
+class FileList extends React.Component{
+  render() {
+    const { classes } = this.props;
+    const preventDefault = event => event.preventDefault();
+    return (
+      <Container component="main" maxWidth="md">
+      <MaterialTable
+        icons={tableIcons}
+        title="File List"
+        columns={[
+          { title: 'File Name', field: 'fName', render: rowData => 
+            <Link href="#" onClick={preventDefault}>
+              Link
+            </Link>
+          },
+          { title: 'Process Name', field: 'pName', render: rowData => 
+            <Link href="#" onClick={preventDefault}>
+              Link
+            </Link>
+          },
+          { title: 'Status', field: 'status' },
+        ]}
+        
+      />
+      </Container>
+    );
+  }
+}
+
+export default withStyles(styles)(FileList);
